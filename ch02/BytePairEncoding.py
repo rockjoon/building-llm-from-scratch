2b53c32742f1c3686af771330f93f5d6dd967e2f
rockjoon <rockpago@gmail.com>
2025-03-11 22:16:52 +0900

    feat: byte pair encoding

---
+++ ch02/BytePairEncoding.py
@@ -0,0 +1,10 @@
+import tiktoken
+
+tokenizer = tiktoken.get_encoding("gpt2")
+text = (
+    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
+    "of someunknownPlace."
+)
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print(integers)
+print(tokenizer.decode(integers))
